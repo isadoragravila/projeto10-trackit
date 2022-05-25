fb47dcc8bfe5961660ab0c9cfacdbbfeeff481d8
isadoragravila <isadoragravila@gmail.com>
2022-05-24 22:54:11 -0300

    react-loader-spinner + loading tela de login e de cadastro

--- src/Components/TelaCadastro.js
+++ src/Components/TelaCadastro.js
@@ -3,45 +3,46 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { useState } from 'react';
 import logo from '../Assets/logo.png';
+import { ThreeDots } from  'react-loader-spinner';
 
 export default function TelaCadastro() {
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [image, setImage] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
 
     function fazerCadastro (event) {
         event.preventDefault();
-        
-        const body = {
-            email,
-            name,
-	        image,
-	        password
-        }
-
+        setIsLoading(true);
+        const body = { email, name, image, password };
         const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up", body);
 
         promise.then(() => {
             navigate("/");
         });
-
         promise.catch(err => {
             alert(err.response.data.message);
+            setIsLoading(false);
         });
     }
+
     return (
         <Conteiner>
             <img src={logo} alt='logo' />
             <h1>TrackIt</h1>
-            <form onSubmit={fazerCadastro}>
-                <input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="senha" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <input type="text" placeholder="nome" value={name} onChange={(e) => setName(e.target.value)} />
-                <input type="url" placeholder="foto" value={image} onChange={(e) => setImage(e.target.value)} />
-                <button type="submit">Cadastrar</button>
-            </form>
+            <Form onSubmit={fazerCadastro}>
+                <Input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} cor={isLoading ? "#F2F2F2" : "#FFFFFF"} />
+                <Input type="password" placeholder="senha" value={password} onChange={(e) => setPassword(e.target.value)} cor={isLoading ? "#F2F2F2" : "#FFFFFF"} />
+                <Input type="text" placeholder="nome" value={name} onChange={(e) => setName(e.target.value)} cor={isLoading ? "#F2F2F2" : "#FFFFFF"} />
+                <Input type="url" placeholder="foto" value={image} onChange={(e) => setImage(e.target.value)} cor={isLoading ? "#F2F2F2" : "#FFFFFF"} />
+                {isLoading ? (
+                    <Button type="submit" disabled opacity={0.7}>{<ThreeDots color={"#ffffff"} width={51} />}</Button>
+                    ) : (
+                    <Button type="submit" opacity={1}>Cadastrar</Button>
+                )}
+            </Form>
             <Link to="/">
                 <Login>
                     Já tem uma conta? Faça login!
@@ -80,4 +81,47 @@ const Login = styled.div`
     font-size: 14px;
     text-decoration-line: underline;
     color: #52B6FF;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    margin-top: 33px;
+    margin-bottom: 25px;
+`;
+
+const Input = styled.input`
+    width: 303px;
+    height: 45px;
+    border: 1px solid #D5D5D5;
+    border-radius: 5px;
+    margin-bottom: 6px;
+    outline: none;
+    font-family: 'Lexend Deca';
+    font-weight: 400;
+    font-size: 20px;
+    text-indent: 10px;
+    color: #AFAFAF;
+    background-color: ${props => props.cor};
+
+    ::placeholder {
+        color: #DBDBDB;
+    }
+`;
+
+const Button = styled.button`
+    width: 303px;
+    height: 45px;
+    background-color: #52B6FF;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+    font-family: 'Lexend Deca';
+    font-weight: 400;
+    font-size: 21px;
+    color: #FFFFFF;
+    opacity: ${props => props.opacity};
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
--- src/Components/TelaLogin.js
+++ src/Components/TelaLogin.js
@@ -3,29 +3,28 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { useState } from 'react';
 import logo from '../Assets/logo.png';
+import { ThreeDots } from  'react-loader-spinner';
 
 export default function TelaLogin() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [token, setToken] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
 
     function fazerLogin (event) {
         event.preventDefault();
-        const body = {
-            email,
-	        password
-        }
-
+        setIsLoading(true);
+        const body = { email, password };
         const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", body);
 
         promise.then(response => {
             setToken(response.data.token);
             navigate("/hoje");
         });
-
         promise.catch(err => {
             alert(err.response.data.message);
+            setIsLoading(false);
         });
     }
 
@@ -33,11 +32,15 @@ export default function TelaLogin() {
         <Conteiner>
             <img src={logo} alt='logo' />
             <h1>TrackIt</h1>
-            <form onSubmit={fazerLogin}>
-                <input type="email" placeholder="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="senha" required value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit">Entrar</button>
-            </form>
+            <Form onSubmit={fazerLogin}>
+                <Input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} cor={isLoading ? "#F2F2F2" : "#FFFFFF"} />
+                <Input type="password" placeholder="senha" value={password} onChange={(e) => setPassword(e.target.value)} cor={isLoading ? "#F2F2F2" : "#FFFFFF"} />
+                {isLoading ? (
+                    <Button type="submit" disabled opacity={0.7}>{<ThreeDots color={"#ffffff"} width={51} />}</Button>
+                    ) : (
+                    <Button type="submit" opacity={1}>Entrar</Button>
+                )}
+            </Form>
             <Link to="/cadastro">
                 <Cadastro>
                     Não tem uma conta? Cadastre-se!
@@ -76,4 +79,47 @@ const Cadastro = styled.div`
     font-size: 14px;
     text-decoration-line: underline;
     color: #52B6FF;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    margin-top: 33px;
+    margin-bottom: 25px;
+`;
+
+const Input = styled.input`
+    width: 303px;
+    height: 45px;
+    border: 1px solid #D5D5D5;
+    border-radius: 5px;
+    margin-bottom: 6px;
+    outline: none;
+    font-family: 'Lexend Deca';
+    font-weight: 400;
+    font-size: 20px;
+    text-indent: 10px;
+    color: #AFAFAF;
+    background-color: ${props => props.cor};
+
+    ::placeholder {
+        color: #DBDBDB;
+    }
+`;
+
+const Button = styled.button`
+    width: 303px;
+    height: 45px;
+    background-color: #52B6FF;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+    font-family: 'Lexend Deca';
+    font-weight: 400;
+    font-size: 21px;
+    color: #FFFFFF;
+    opacity: ${props => props.opacity};
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
