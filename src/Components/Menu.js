8a2d8b704094a2d213178759679f069608e2879f
isadoragravila <isadoragravila@gmail.com>
2022-05-26 16:21:11 -0300

    Add da barra de progresso (sem % mutável ainda)

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,15 +1,28 @@
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import "react-circular-progressbar/dist/styles.css";
 
-export default function Menu () {
+export default function Menu() {
+    const percentage = 60; //mudar com estado
     return (
         <Conteiner>
             <Link to="/habitos">
                 <p>Hábitos</p>
             </Link>
-            <Link to="/hoje">
-                <p>Hoje</p>
-            </Link>
+            <Progresso>
+                <Link to="/hoje">
+                    <CircularProgressbar value={percentage} text={"Hoje"} background backgroundPadding={6} styles={buildStyles({
+                        backgroundColor: "#52B6FF",
+                        textColor: "#ffffff",
+                        pathColor: "#ffffff",
+                        textSize: "18px",
+                        trailColor: "transparent",
+                        textFamily: "arial"
+                    })}
+                    />
+                </Link>
+            </Progresso>
             <Link to="/historico">
                 <p>Histórico</p>
             </Link>
@@ -25,7 +38,7 @@ const Conteiner = styled.div`
     position: fixed;
     bottom: 0;
     display: flex;
-    align-items: center;
+    align-items: flex-end;
     justify-content: space-between;
     padding: 0 31px;
 
@@ -38,5 +51,15 @@ const Conteiner = styled.div`
         font-weight: 400;
         font-size: 18px;
         color: #52B6FF;
+        margin-bottom: 26px;
     }
+`;
+
+const Progresso = styled.div`
+    width: 91px;
+    padding-bottom: 10px;
+    font-family: 'Lexend Deca';
+    font-weight: 400;
+    font-size: 18px;
+    color: #FFFFFF;
 `;
