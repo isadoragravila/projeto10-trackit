d295a71bec9dc5ed4c0058a43c1b3b6eb38697ad
isadoragravila <isadoragravila@gmail.com>
2022-05-30 18:54:55 -0300

    Tela histórico - Bônus clique no dia

--- src/Components/TelaHistorico.js
+++ src/Components/TelaHistorico.js
@@ -9,11 +9,23 @@ import Calendar from 'react-calendar';
 import "react-calendar/dist/Calendar.css";
 import dayjs from 'dayjs';
 
-export default function TelaHistorico () {
+function Historico({ habits, day }) {
+    return (
+        <ListaHabitos>
+            {day}
+            {habits.map(habito => <Habito key={habito.id} cor={habito.done ? "#8FC549" : "#ea5766"}>{habito.name}</Habito>)}
+        </ListaHabitos>
+    );
+}
+
+export default function TelaHistorico() {
     const navigate = useNavigate();
     const { token } = useContext(TokenContext);
     const [historico, setHistorico] = useState([]);
-    
+    const [clicked, setClicked] = useState(false);
+    const [habits, setHabits] = useState([]);
+    const [day, setDay] = useState('');
+
     useEffect(() => {
         if (!token) {
             navigate("/");
@@ -35,16 +47,27 @@ export default function TelaHistorico () {
 
     function tileClassName({ date, view }) {
         if (view === "month") {
-          if (historico.find((d) => d.day === dayjs(date).format("DD/MM/YYYY"))) {
             const dia = historico.find((d) => d.day === dayjs(date).format("DD/MM/YYYY"));
-            const verif = dia.habits.filter((d) => d.done === false);
-            if (verif.length === 0) {
-              return "feito";
+            if (dia) {
+                const naoFeitos = dia.habits.filter((d) => d.done === false);
+                if (naoFeitos.length === 0) {
+                    return "feito";
+                }
+                return "nao-feito";
             }
-            return "nao-feito";
-          }
         }
-      }
+    }
+
+    function clickDia(value) {
+        const dia = historico.find((d) => d.day === dayjs(value).format("DD/MM/YYYY"));
+        if (dia) {
+            setClicked(true);
+            setHabits(dia.habits);
+            setDay(dayjs(value).format("DD/MM/YYYY"));
+        } else {
+            setClicked(false);
+        }
+    }
 
     return (
         <>
@@ -52,7 +75,8 @@ export default function TelaHistorico () {
             <Conteiner>
                 <Content>
                     <h2>Histórico</h2>
-                    <Calendar locale="pt-br" className="calendario" tileClassName={tileClassName}/>
+                    <Calendar locale="pt-br" className="calendario" tileClassName={tileClassName} onClickDay={clickDia} />
+                    {clicked ? <Historico habits={habits} day={day} /> : null}
                 </Content>
             </Conteiner>
             <Menu />
@@ -62,7 +86,8 @@ export default function TelaHistorico () {
 
 const Conteiner = styled.div`
     margin-top: 70px;
-    height: 100vh;
+    margin-bottom: 70px;
+    min-height: 100vh;
     background-color: #E5E5E5;
     max-width: 500px;
     min-width: 375px;
@@ -86,4 +111,23 @@ const Content = styled.div`
         line-height: 22px;
         color: #666666;
     }
+`;
+
+const ListaHabitos = styled.div`
+    width: 100%;
+    margin-top: 15px;
+    background-color: #FFFFFF;
+    border-radius: 5px;
+    font-family: 'Lexend Deca';
+    font-weight: 400;
+    font-size: 16px;
+    color: #666666;
+    text-indent: 18px;
+    padding-top: 18px;
+`;
+
+const Habito = styled.div`
+    width: 100%;
+    padding: 18px 0;
+    color: ${props => props.cor};
 `;
